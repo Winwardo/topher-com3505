d895a87179feb2d99a4195b8443c7ebdaad517ae
Topher Winward <hi@topher.io>
2015-10-22 12:49:23 +0100

    Node GL injection

--- src/scenegraph/Node.java
+++ src/scenegraph/Node.java
@@ -3,6 +3,8 @@ package scenegraph;
 import java.util.List;
 import java.util.Optional;
 
+import com.jogamp.opengl.GL2;
+
 import main.Vector3;
 import renderer.Renderable;
 
@@ -11,24 +13,32 @@ public class Node implements Renderable {
 	private List<Node> children;
 	private Vector3 localPosition;
 	private Vector3 localRotation;
+	private GL2 gl;
 
-	public Node(Optional<Renderable> toRender, List<Node> children, Vector3 localPosition, Vector3 localRotation) {
+	public Node(Optional<Renderable> toRender, List<Node> children, Vector3 localPosition, Vector3 localRotation,
+			GL2 gl) {
 		super();
 		this.toRender = toRender;
 		this.children = children;
 		this.localPosition = localPosition;
 		this.localRotation = localRotation;
+		this.gl = gl;
 	}
 
 	@Override
 	public void render() {
-		if (toRender.isPresent()) {
-			toRender.get().render();
-		}
+		gl.glPushMatrix();
+		{
+			// rotate and translate
+			if (toRender.isPresent()) {
+				toRender.get().render();
+			}
 
-		for (Node child : children) {
-			child.render();
+			for (Node child : children) {
+				child.render();
+			}
 		}
+		gl.glPopMatrix();
 	}
 
 }
