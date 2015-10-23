ff8d998e0b47873f971a19293d95db3acfc215be
Topher Winward <hi@topher.io>
2015-10-23 16:50:55 +0100

    Head wobbles

--- src/scenegraph/models/robot/Head.java
+++ src/scenegraph/models/robot/Head.java
@@ -14,35 +14,45 @@ public class Head extends SceneGraph {
     private GL2  gl;
     private GLUT glut;
 
+    private final SceneGraphNode pitch;
+    private final SceneGraphNode yaw;
+    private final SceneGraphNode roll;
+
     public Head(GL2 gl, GLUT glut) {
         super(new SceneGraphNode(gl));
         this.gl = gl;
         this.glut = glut;
 
+        pitch = root.setRotation(new Vector3(0, 0, 1), 0);
+        yaw = pitch.createAttachedNode().setRotation(new Vector3(0, 1, 0), 0);
+        roll = yaw.createAttachedNode().setRotation(new Vector3(1, 0, 0), 0);
+
+        SceneGraphNode wobblerNode = roll.createAttachedNode();
+
         // Core head
-        root.setScaling(new Vector3(0.6f, 1, 1));
-        root.attachRenderable(new Teapot(gl, glut));
+        wobblerNode.setScaling(new Vector3(0.6f, 1, 1));
+        wobblerNode.attachRenderable(new Teapot(gl, glut));
 
-        attachLeftEye();
-        attachRightEye();
+        attachLeftEye(wobblerNode);
+        attachRightEye(wobblerNode);
     }
 
-    private void attachLeftEye() {
-        attachEye(-eyeOffset);
+    private void attachLeftEye(SceneGraphNode head) {
+        attachEye(-eyeOffset, head);
     }
 
-    private void attachRightEye() {
-        attachEye(eyeOffset);
+    private void attachRightEye(SceneGraphNode head) {
+        attachEye(eyeOffset, head);
     }
 
-    public void attachEye(float xOffset) {
-        SceneGraphNode eye = root.createAttachedNode();
+    public void attachEye(float xOffset, SceneGraphNode head) {
+        SceneGraphNode eye = head.createAttachedNode();
         eye.attachRenderable(new Sphere(gl, glut));
         eye.setScaling(Vector3.all(0.2f));
         eye.setPosition(new Vector3(0.7f, 0.2f, xOffset));
 
         {
-            SceneGraphNode pupil = root.createAttachedNode();
+            SceneGraphNode pupil = head.createAttachedNode();
             pupil.attachRenderable(new Sphere(gl, glut));
             pupil.setScaling(Vector3.all(0.25f));
             pupil.setPosition(new Vector3(0.5f, 0, 0));
@@ -51,5 +61,13 @@ public class Head extends SceneGraph {
 
     @Override
     public void update() {
+        float now = System.currentTimeMillis() % 100000;
+        float smaller = now * 0.001f;
+        float rotate = (float) Math.sin(smaller);
+
+        // wobbler.update();
+
+        yaw.setRotationAmount((float) Math.sin(smaller) * 25);
+        pitch.setRotationAmount((float) Math.cos(smaller * 1.5f) * 15);
     }
 }
--- src/scenegraph/models/robot/Robot.java
+++ src/scenegraph/models/robot/Robot.java
@@ -7,23 +7,37 @@ import scenegraph.SceneGraph;
 import scenegraph.SceneGraphNode;
 
 public class Robot extends SceneGraph {
-    private GL2  gl;
-    private GLUT glut;
+    private static final float ARM_OUT = 0.65f;
+    private GL2                gl;
+    private GLUT               glut;
 
     private final SceneGraph roller;
+    private final SceneGraph head;
 
     public Robot(GL2 gl, GLUT glut) {
         super(new SceneGraphNode(gl));
         this.gl = gl;
         this.glut = glut;
 
-        SceneGraphNode head = root
-            .createAttachedNodeFromSceneGraph(new Head(gl, glut));
-        head.setPosition(new Vector3(0, 3, 0));
+        head = new Head(gl, glut);
+        root.createAttachedNodeFromSceneGraph(head).setPosition(
+            new Vector3(0, 3, 0));
 
-        SceneGraphNode body = root
-            .createAttachedNodeFromSceneGraph(new Body(gl, glut));
-        body.setPosition(new Vector3(0, 2.3f, 0));
+        root.createAttachedNodeFromSceneGraph(new Body(gl, glut)).setPosition(
+            new Vector3(0, 2.3f, 0));
+
+        SceneGraphNode arms = root
+            .createAttachedNode()
+            .setPosition(new Vector3(0, 2.0f, 0));
+
+        SceneGraphNode leftArm = arms
+            .createAttachedNodeFromSceneGraph(new Arm(gl, glut));
+        leftArm.setPosition(new Vector3(0, 0, ARM_OUT));
+
+        SceneGraphNode rightArm = arms
+            .createAttachedNodeFromSceneGraph(new Arm(gl, glut));
+        rightArm.setPosition(new Vector3(0, 0, -ARM_OUT));
+        rightArm.setRotation(new Vector3(1, 0, 0), 180);
 
         roller = new Roller(gl, glut);
         root.createAttachedNodeFromSceneGraph(roller);
@@ -32,5 +46,6 @@ public class Robot extends SceneGraph {
     @Override
     public void update() {
         roller.update();
+        head.update();
     }
 }
