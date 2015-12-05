44865248dc5d4221ce9b5644f3ba62e6861a4bf5
Topher Winward <hi@topher.io>
2015-12-05 22:47:05 +0000

    Sphere displayList

--- src/renderer/primitives/Sphere.java
+++ src/renderer/primitives/Sphere.java
@@ -3,6 +3,7 @@ package renderer.primitives;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.glu.GLUquadric;
+import renderer.DisplayList;
 import renderer.Material;
 import renderer.Renderable;
 
@@ -11,8 +12,7 @@ public class Sphere extends Renderable {
 
     private static final int   SPHERE_SUBDIVISION    = 20;
 
-    private float              radius;
-    private int                mysphereID;
+    private final DisplayList  displayList;
 
     public Sphere(GL2 gl) {
         this(gl, SPHERE_DEFAULT_RADIUS, Material.empty(gl));
@@ -24,26 +24,27 @@ public class Sphere extends Renderable {
 
     public Sphere(GL2 gl, float radius, Material mat) {
         super(gl, mat);
-        this.radius = radius;
 
-        GLU glu = new GLU();
+        displayList = new DisplayList(gl, (x) -> {
+            GLU glu = new GLU();
 
-        GLUquadric sphere = glu.gluNewQuadric();
+            GLUquadric sphere = glu.gluNewQuadric();
 
-        glu.gluQuadricDrawStyle(sphere, glu.GLU_FILL);
-        glu.gluQuadricTexture(sphere, true);
-        glu.gluQuadricNormals(sphere, glu.GLU_SMOOTH);
-        // Making a display list
-        mysphereID = gl.glGenLists(1);
-        gl.glNewList(mysphereID, gl.GL_COMPILE);
+            glu.gluQuadricDrawStyle(sphere, glu.GLU_FILL);
+            glu.gluQuadricTexture(sphere, true);
+            glu.gluQuadricNormals(sphere, glu.GLU_SMOOTH);
 
-        glu.gluSphere(sphere, radius, SPHERE_SUBDIVISION, SPHERE_SUBDIVISION);
-        gl.glEndList();
-        glu.gluDeleteQuadric(sphere);
+            glu.gluSphere(
+                sphere,
+                radius,
+                SPHERE_SUBDIVISION,
+                SPHERE_SUBDIVISION);
+            glu.gluDeleteQuadric(sphere);
+        });
     }
 
     @Override
     public void renderImpl() {
-        gl.glCallList(mysphereID);
+        displayList.call();
     }
 }
